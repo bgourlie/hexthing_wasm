2850bffa13aff97b051bef8356abc64fb2fe2382
W. Brian Gourlie <bgourlie@gmail.com>
2018-10-21 18:02:17 -0500

    Specs is working

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -2,15 +2,25 @@
 
 #[macro_use]
 extern crate cfg_if;
-
 extern crate wasm_bindgen;
 extern crate js_sys;
+extern crate specs;
 extern crate web_sys;
+extern crate rand;
 
+use rand::Rng;
+use rand::distributions::{Distribution};
+use specs::prelude::*;
+use specs::storage::HashMapStorage;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{JsCast, JsValue};
 use js_sys::{WebAssembly};
-use web_sys::{WebGlProgram, WebGl2RenderingContext, WebGlShader};
+use web_sys::{console, WebGlProgram, WebGl2RenderingContext, WebGlShader};
+
+
+macro_rules! console_log {
+    ($($t:tt)*) => (console::log_1(JsValue::from_str(&format_args!($($t)*).to_string()).as_ref());)
+}
 
 cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
@@ -28,6 +38,107 @@ cfg_if! {
         extern crate wee_alloc;
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+const TAU: f32 = 2. * std::f32::consts::PI;
+
+#[derive(Debug)]
+struct ClusterBomb {
+    fuse: usize,
+}
+impl Component for ClusterBomb {
+    // This uses `HashMapStorage`, because only some entities are cluster bombs.
+    type Storage = HashMapStorage<Self>;
+}
+
+#[derive(Debug)]
+struct Shrapnel {
+    durability: usize,
+}
+impl Component for Shrapnel {
+    // This uses `HashMapStorage`, because only some entities are shrapnels.
+    type Storage = HashMapStorage<Self>;
+}
+
+#[derive(Debug, Clone)]
+struct Pos(f32, f32);
+impl Component for Pos {
+    // This uses `VecStorage`, because all entities have a position.
+    type Storage = VecStorage<Self>;
+}
+
+#[derive(Debug)]
+struct Vel(f32, f32);
+impl Component for Vel {
+    // This uses `DenseVecStorage`, because nearly all entities have a velocity.
+    type Storage = DenseVecStorage<Self>;
+}
+
+struct ClusterBombSystem;
+impl<'a> System<'a> for ClusterBombSystem {
+    type SystemData = (
+        Entities<'a>,
+        WriteStorage<'a, ClusterBomb>,
+        ReadStorage<'a, Pos>,
+        // Allows lazily adding and removing components to entities
+        // or executing arbitrary code with world access lazily via `execute`.
+        Read<'a, LazyUpdate>,
+    );
+
+    fn run(&mut self, (entities, mut bombs, positions, updater): Self::SystemData) {
+        // Join components in potentially parallel way using rayon.
+        (&entities, &mut bombs, &positions)
+            .join()
+            .for_each(|(entity, bomb, position)| {
+                let mut rng = rand::thread_rng();
+
+                if bomb.fuse == 0 {
+                    let _ = entities.delete(entity);
+                    for _ in 0..9 {
+                        let shrapnel = entities.create();
+                        updater.insert(
+                            shrapnel,
+                            Shrapnel {
+                                durability: rng.gen_range(10, 20),
+                            },
+                        );
+                        updater.insert(shrapnel, position.clone());
+                        let angle: f32 = rng.gen::<f32>() * TAU;
+                        updater.insert(shrapnel, Vel(angle.sin(), angle.cos()));
+                    }
+                } else {
+                    bomb.fuse -= 1;
+                }
+            });
+    }
+}
+
+struct PhysicsSystem;
+impl<'a> System<'a> for PhysicsSystem {
+    type SystemData = (WriteStorage<'a, Pos>, ReadStorage<'a, Vel>);
+
+    fn run(&mut self, (mut pos, vel): Self::SystemData) {
+        (&mut pos, &vel).join().for_each(|(pos, vel)| {
+            pos.0 += vel.0;
+            pos.1 += vel.1;
+        });
+    }
+}
+
+struct ShrapnelSystem;
+impl<'a> System<'a> for ShrapnelSystem {
+    type SystemData = (Entities<'a>, WriteStorage<'a, Shrapnel>);
+
+    fn run(&mut self, (entities, mut shrapnels): Self::SystemData) {
+        (&entities, &mut shrapnels)
+            .join()
+            .for_each(|(entity, shrapnel)| {
+                if shrapnel.durability == 0 {
+                    let _ = entities.delete(entity);
+                } else {
+                    shrapnel.durability -= 1;
+                }
+            });
     }
 }
 
@@ -95,8 +206,64 @@ pub fn draw() {
         0,
         (vertices.len() / 3) as i32,
     );
-}
 
+    let mut world = World::new();
+
+    let mut dispatcher = DispatcherBuilder::new()
+        .with(PhysicsSystem, "physics", &[])
+        .with(ClusterBombSystem, "cluster_bombs", &[])
+        .with(ShrapnelSystem, "shrapnels", &[])
+        .build();
+
+    dispatcher.setup(&mut world.res);
+
+    world
+        .create_entity()
+        .with(Pos(0., 0.))
+        .with(ClusterBomb { fuse: 3 })
+        .build();
+
+    let mut step = 0;
+    loop {
+        step += 1;
+        let mut entities = 0;
+        {
+            // Simple console rendering
+            let positions = world.read_storage::<Pos>();
+            const WIDTH: usize = 10;
+            const HEIGHT: usize = 10;
+            const SCALE: f32 = 1. / 4.;
+            let mut screen = [[0; WIDTH]; HEIGHT];
+            for entity in world.entities().join() {
+                if let Some(pos) = positions.get(entity) {
+                    let x = (pos.0 * SCALE + WIDTH as f32 / 2.).floor() as usize;
+                    let y = (pos.1 * SCALE + HEIGHT as f32 / 2.).floor() as usize;
+                    if x < WIDTH && y < HEIGHT {
+                        screen[x][y] += 1;
+                    }
+                }
+                entities += 1;
+            }
+            console_log!("Step: {}, Entities: {}", step, entities);
+            for row in &screen {
+                let mut line = String::with_capacity(10);
+                for cell in row {
+                    line.push(format!("{}", cell).chars().next().unwrap());
+                }
+                console_log!("{}", line);
+            }
+        }
+        if entities == 0 {
+            break;
+        }
+
+        dispatcher.dispatch(&world.res);
+
+        // Maintain dynamically added and removed entities in dispatch.
+        // This is what actually executes changes done by `LazyUpdate`.
+        world.maintain();
+    }
+}
 pub fn compile_shader(
     context: &WebGl2RenderingContext,
     shader_type: u32,

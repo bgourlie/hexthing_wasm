e5f697a4ecf4cb6e27f24d31313f7e12486a7036
W. Brian Gourlie <bgourlie@gmail.com>
2018-10-21 14:52:41 -0500

    Use web-sys

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -4,7 +4,11 @@
 extern crate cfg_if;
 
 extern crate wasm_bindgen;
+extern crate web_sys;
+
 use wasm_bindgen::prelude::*;
+use web_sys::{Node};
+
 
 cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
@@ -25,36 +29,11 @@ cfg_if! {
     }
 }
 
-// Definitions of the functionality available in JS, which wasm-bindgen will
-// generate shims for today (and eventually these should be near-0 cost!)
-//
-// These definitions need to be hand-written today but the current vision is
-// that we'll use WebIDL to generate this `extern` block into a crate which you
-// can link and import. There's a tracking issue for this at
-// https://github.com/rustwasm/wasm-bindgen/issues/42
-//
-// In the meantime these are written out by hand and correspond to the names and
-// signatures documented on MDN, for example
-#[wasm_bindgen]
-extern "C" {
-    type HTMLDocument;
-    static document: HTMLDocument;
-    #[wasm_bindgen(method)]
-    fn createElement(this: &HTMLDocument, tagName: &str) -> Element;
-    #[wasm_bindgen(method, getter)]
-    fn body(this: &HTMLDocument) -> Element;
-
-    type Element;
-    #[wasm_bindgen(method, setter = innerHTML)]
-    fn set_inner_html(this: &Element, html: &str);
-    #[wasm_bindgen(method, js_name = appendChild)]
-    fn append_child(this: &Element, other: Element);
-}
-
 // Called by our JS entry point to run the example
 #[wasm_bindgen]
 pub fn run() {
-    let val = document.createElement("p");
-    val.set_inner_html("Hello from Rust, WebAssembly, and Webpack!");
-    document.body().append_child(val);
+    let document = web_sys::window().unwrap().document().unwrap();
+    let val = document.create_element("p").unwrap();
+    val.set_inner_html("Hello from Rust, WebAssembly, WebPack, and web-sys!");
+    AsRef::<Node>::as_ref(&document.body().unwrap()).append_child(AsRef::<Node>::as_ref(&val));
 }
